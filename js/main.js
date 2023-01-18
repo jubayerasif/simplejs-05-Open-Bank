afc89022a141233fd6b4d87d94c7a78d8e12a9d5
jubayerasif <jubaerasif101@gmail.com>
2023-01-19 00:36:13 +0600

    making js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
-const loginBtn = document.getElementById('login-btn').addEventListener('click', function(event){
+// login-btn even handler
+const loginBtn = document.getElementById('login-btn').addEventListener('click', function(){
     const loginArea = document.getElementById('login-area').style.display = 'none';
     const transactionArea = document.getElementById('transaction-area').style.display = 'block';
 })
 
+//deposit btn event handler
+const depositBtn = document.getElementById('addDeposit').addEventListener('click', function(){
+    const depositedAmount = document.getElementById('depositedAmount').value;
+})
 
 
 
